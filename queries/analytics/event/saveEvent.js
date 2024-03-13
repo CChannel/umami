db5861416782a39305c34054f0eead8a4c4697e2
Anup Naik <anupnaik1@gmail.com>
2024-03-13 21:08:35 +0900

    add event data to firehose

--- queries/analytics/event/saveEvent.js
+++ queries/analytics/event/saveEvent.js
@@ -2,6 +2,7 @@ import { EVENT_NAME_LENGTH, URL_LENGTH } from 'lib/constants';
 import { CLICKHOUSE, PRISMA, runQuery } from 'lib/db';
 import kafka from 'lib/kafka';
 import prisma from 'lib/prisma';
+import putRecordToKinesisFirehose, { EVENT_STREAM } from 'lib/firehose';
 
 export async function saveEvent(...args) {
   return runQuery({
@@ -27,9 +28,13 @@ async function relationalQuery(website_id, { session_id, url, event_name, event_
     };
   }
 
-  return prisma.client.event.create({
+  const eventData = await prisma.client.event.create({
     data,
   });
+
+  putRecordToKinesisFirehose({ eventData }, EVENT_STREAM);
+
+  return eventData;
 }
 
 async function clickhouseQuery(
